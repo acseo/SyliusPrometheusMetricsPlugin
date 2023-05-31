7cd6366bd2744b23c4bdb839d6412419f1b648d7
Sabrine FERCHICHI <sabrine.ferchichi@acseo-conseil.fr>
2023-05-31 16:48:20 +0200

    Enable Prometheus Metrics

---
+++ src/Collector/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acseo\SyliusPrometheusMetricsPlugin\Collector;
+
+use Prometheus\CollectorRegistry;
+use Sylius\Component\Core\Model\OrderInterface;
+
+class Order
+{
+    public function __construct(
+        protected string $prometheusMetricsNamespace,
+        protected CollectorRegistry $collectionRegistry
+    ) {
+    }
+
+    public function incrementPrometheusCounters(OrderInterface $order): void
+    {
+        $orderCounter = $this->collectionRegistry->getOrRegisterCounter(
+            $this->prometheusMetricsNamespace,
+            'order_complete',
+            'amount of completed orders',
+            ['order']
+        );
+        $orderCounter->inc(['all']);
+
+        $amountCounter = $this->collectionRegistry->getOrRegisterCounter(
+            $this->prometheusMetricsNamespace,
+            'order_amount',
+            'amount of completed orders',
+            ['order']
+        );
+        $amountCounter->incBy($order->getTotal(), ['all']);
+    }
+}
---
+++ src/SyliusPrometheusMetricsPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acseo\SyliusPrometheusMetricsPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SyliusPrometheusMetricsPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
